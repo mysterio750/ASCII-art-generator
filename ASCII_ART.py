acf913de64dca0b20ecf8a44b33fdb40a94f9091
Krish <krishkumarchauhan57@gmail.com>
2026-06-02 14:56:40 +0530

    Update ASCII_ART.py

--- ASCII_ART.py
+++ ASCII_ART.py
@@ -1,8 +1,10 @@
 from PIL import Image
-img = Image.open(r"C:\Users\Asus\OneDrive\Pictures\Screenshots\Screenshot 2026-04-08 034543.png")
-new_width = 150
-new_height = 50
+img = Image.open(r" ") # <<< your image path goes here
+new_width = 300
+new_height = 100
+
 tiny_img = img.resize((new_width, new_height))
+
 grayscale_img = tiny_img.convert("L")
 pixel_data = list((grayscale_img.getdata()))
 idk =[]
@@ -39,8 +41,8 @@ for i in pixel_data:
     if 226 <= j <= 255:
         i = "@"
         idk.append(i)
-final = "".join(idk)   
-i = 0
+final = "".join(idk) 
+i = 0  
 while i < len(final):
     print(final[i:i+new_width])
     i = i + new_width
